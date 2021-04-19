448d9fc4d06c4f438db51c1261a63b27024efd03
Hojoon Lee <40584543+ethos93@users.noreply.github.com>
2021-04-19 11:37:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const os = require('os');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('This hostname is ' + os.hostname() + '\n');
+  res.end('This hostname is ' + os.hostname() + '!!!\n');
 });
 
 server.listen(8080, '0.0.0.0', () => {
